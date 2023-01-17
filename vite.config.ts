fd4c3ac7e0839a0369679d72f1e9682e2f048969
He110te4m <me@he110.site>
2023-01-17 11:28:29 +0800

    feat: add eslint and vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,60 @@
-import { defineConfig } from 'vite';
-import { basename, join, relative, resolve } from 'path';
-import { readdirSync, statSync } from 'fs';
+/// <reference types="vitest" />
+
+import { basename, join, relative, resolve } from 'path'
+import { readdirSync, statSync } from 'fs'
+import { defineConfig } from 'vite'
 
 const tsSuffix = '.ts'
 
 export default defineConfig({
   build: {
     emptyOutDir: false,
-    outDir: "dist",
+    outDir: 'dist',
     sourcemap: true,
     lib: {
-      formats: ["cjs", "es"],
+      formats: ['cjs', 'es'],
       entry: getEntries(),
     },
   },
-});
+  test: {
+    coverage: {
+      provider: 'c8',
+      reporter: ['json', 'html'],
+      reportsDirectory: 'coverages',
+      include: [
+        'src/**/*.ts',
+      ],
+    },
+  },
+})
 
 /**
  * get library entries
  */
 function getEntries() {
   // get entries
-  const initEntries: Record<string, string> = {};
-  const sourceDirectory = getSourceDirectory();
-  const modules = getModules(sourceDirectory);
+  const initEntries: Record<string, string> = {}
+  const sourceDirectory = getSourceDirectory()
+  const modules = getModules(sourceDirectory)
   const entries = modules.reduce((obj, entryName) => {
     const relPath = relative(__dirname, sourceDirectory)
-    obj[entryName] = join(relPath, entryName + tsSuffix);
-    return obj;
-  }, initEntries);
+    obj[entryName] = join(relPath, entryName + tsSuffix)
+    return obj
+  }, initEntries)
 
-  return entries;
+  return entries
 }
 
 function getSourceDirectory() {
-  return resolve(__dirname, 'src');
+  return resolve(__dirname, 'src')
 }
 
 function getModules(directory: string) {
   return readdirSync(directory)
-    .filter(filename => {
-      const fullPath = resolve(directory, filename);
-      const stat = statSync(fullPath);
-      return stat.isFile();
+    .filter((filename) => {
+      const fullPath = resolve(directory, filename)
+      const stat = statSync(fullPath)
+      return stat.isFile()
     })
-    .map(filename => basename(filename, tsSuffix));
+    .map(filename => basename(filename, tsSuffix))
 }
